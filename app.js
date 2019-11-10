09eab1ff22db0130123778b824f00c559aa62def
jonestrada7 <jonathan.estrada747@gmail.com>
2019-11-10 09:46:36 -0800

    Added dev tools

--- app.js
+++ app.js
@@ -1,30 +1,25 @@
-var express = require('express');
-var app = express(); 
+const express = require("express");
+const app = express();
 
 app.use(express.json());
 
-app.get("/", function(req, res){
-	res.render('home'); 
-}); 
-
-app.get("/search", function(req, res){
-	res.render('search')
-	console.log("Search page has been activated");
+app.get("/", function(req, res) {
+  res.render("home");
 });
 
-app.get("/mybobalist", function(req, res){
-	res.render('bobalist')
-	console.log("Bobalist page has been activated");
+app.get("/search", function(req, res) {
+  res.render("search");
+  console.log("Search page has been activated");
 });
 
-app.post("/mybobalist", function(req, res){
-
+app.get("/mybobalist", function(req, res) {
+  res.render("bobalist");
+  console.log("Bobalist page has been activated");
 });
 
-
-
+app.post("/mybobalist", function(req, res) {});
 
 var port = 3000;
-app.listen(port, function(){
-	console.log(`App running on port ~{port}...`);
-}); 
+app.listen(port, function() {
+  //   console.log(`App running on port ${port}...`);
+});
