66328bc37277928c6e15a20d5dd0428f50593cba
terryhn1 <57304245+terryhn1@users.noreply.github.com>
2019-11-10 09:30:03 -0800

    Add files via upload
    Server Beginning

---
+++ app (1).js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express(); 
+
+app.use(express.json());
+
+app.get("/", function(req, res){
+	res.render('home'); 
+}); 
+
+app.get("/search", function(req, res){
+	res.render('search')
+	console.log("Search page has been activated");
+});
+
+app.get("/mybobalist", function(req, res){
+	res.render('bobalist')
+	console.log("Bobalist page has been activated");
+});
+
+app.post("/mybobalist", function(req, res){
+
+});
+
+
+
+
+var port = 3000;
+app.listen(port, function(){
+	console.log(`App running on port ~{port}...`);
+}); 
